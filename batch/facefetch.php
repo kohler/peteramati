cd64b0ff9fcd6ffecb434787758a56d5e89618b5
Eddie Kohler <kohler@seas.harvard.edu>
2015-09-30 14:20:34 +0000

    Fix facefetch script.

--- batch/facefetch.php
+++ batch/facefetch.php
@@ -7,7 +7,7 @@ $ConfSiteBase = preg_replace(',/batch/[^/]+,', '', __FILE__);
 require_once("$ConfSiteBase/src/init.php");
 require_once("$ConfSiteBase/lib/getopt.php");
 
-$arg = getopt_rest($argv, "hn:l:", array("help", "name:", "limit:"));
+$arg = getopt_rest($argv, "hn:l:a", array("help", "name:", "limit:", "all"));
 if (isset($arg["h"]) || isset($arg["help"]) || count($arg["_"]) > 1) {
     fwrite(STDOUT, "Usage: php batch/facefetch.php [-l LIMIT] [FETCHSCRIPT]\n");
     exit(0);
@@ -33,15 +33,26 @@ if (!isset($arg["limit"]))
     $arg["limit"] = @$arg["l"];
 $limit = (int) $arg["limit"];
 
-$result = Dbl::qe("select contactId, email, huid from ContactInfo where huid is not null and contactImageId is null");
-$n = $nworked = 0;
-while (($row = edb_row($result))) {
-    $url = $facefetch_urlpattern;
-    $url = str_replace('${ID}', urlencode($row[2]), $url);
-    $url = str_replace('${EMAIL}', urlencode($row[1]), $url);
-    fwrite(STDOUT, $row[1] . " ");
+$result = Dbl::qe("select contactId, email, huid from ContactInfo" . (isset($arg["all"]) || isset($arg["a"]) ? "" : " where contactImageId is null"));
+$rows = array();
+while (($row = edb_row($result)))
+    $rows[] = $row;
+if ($limit)
+    shuffle($rows);
+Dbl::free($result);
 
-    $handle = popen($fetchscript . " -q " . escapeshellarg($url), "r");
+$n = $nworked = 0;
+foreach ($rows as $row) {
+    $url = $facefetch_urlpattern;
+    if (($row[1] === null && strpos($url, '${EMAIL}') !== false)
+        || ($row[2] === null && strpos($url, '${ID}') !== false))
+        continue;
+    $url = str_replace('${EMAIL}', urlencode($row[1]), $url);
+    $url = str_replace('${ID}', urlencode($row[2]), $url);
+    // fwrite(STDOUT, "$row[1] $fetchscript " . escapeshellarg($url) . " ");
+    fwrite(STDOUT, "$row[1] ");
+
+    $handle = popen($fetchscript . " " . escapeshellarg($url), "r");
     $data = stream_get_contents($handle);
     $status = pclose($handle);
 
@@ -54,7 +65,7 @@ while (($row = edb_row($result))) {
     $worked = false;
     if (pcntl_wifexited($status) && pcntl_wexitstatus($status) == 0
         && preg_match(',\Aimage/,', $content_type)) {
-        $sresult = Dbl::fetch_first_object(Dbl::qe("select ContactImage.* from ContactImage join ContactInfo using (contactImageId) where contactId=?", $row[0]));
+        $sresult = Dbl::fetch_first_object(Dbl::qe("select ContactImage.* from ContactImage join ContactInfo using (contactImageId) where ContactInfo.contactId=?", $row[0]));
         if ($sresult && $sresult->mimetype === $content_type && $sresult->data === $data)
             $worked = "unchanged";
         else {
