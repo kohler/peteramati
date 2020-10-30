8aa8e2cdb6e0e4910001e28a8945752610d8d78d
Eddie Kohler <ekohler@gmail.com>
2020-10-30 18:27:28 -0400

    Rollup: Add `?mtime=` argument to sourcemap to ensure it's cacheable.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,24 @@
 import { terser } from "rollup-plugin-terser";
+const fs = require("fs");
+
+// append mtime to sourcemap
+const sourcemap_mtime = {
+    name: "append-sourcemap-mtime",
+    writeBundle() {
+        const mtime = Math.trunc(fs.statSync("scripts/pa.min.js.map").mtime.getTime() / 1000);
+        const fd = fs.openSync("scripts/pa.min.js", "r+");
+        const sz = fs.fstatSync(fd).size;
+        const buf = Buffer.alloc(8192);
+        const nr = fs.readSync(fd, buf, 0, 1000, Math.max(sz - 1000, 0));
+        const s = buf.toString("utf8", 0, nr);
+        const m = s.match(/^([^]*\/\/\# sourceMappingURL=pa\.min\.js\.map)\s*$/);
+        if (m) {
+            const nbuf = Buffer.from(m[1] + "?mtime=" + mtime + "\n", "utf8");
+            fs.writeSync(fd, nbuf, 0, nbuf.length, Math.max(sz - 1000, 0));
+        }
+        fs.closeSync(fd);
+    }
+};
 
 export default [{
     input: "scriptsrc/main.js",
@@ -7,7 +27,7 @@ export default [{
         format: "iife",
         sourcemap: true,
         sourcemapExcludeSources: true,
-        plugins: [terser()],
+        plugins: [terser(), sourcemap_mtime],
         name: "$pa"
     }
 }];
