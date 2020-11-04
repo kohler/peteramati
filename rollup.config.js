51fa6e6e93e23fdff74fc70eafda8fdd877a9d79
Eddie Kohler <ekohler@gmail.com>
2020-11-04 10:36:58 -0500

    Avoid spurious minified-JS updates with SHA256 hash of sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
 import { terser } from "rollup-plugin-terser";
 import { eslint } from "rollup-plugin-eslint";
 const fs = require("fs");
+const crypto = require("crypto");
+
 
 // append mtime to sourcemap
 const sourcemap_mtime = {
     name: "append-sourcemap-mtime",
     writeBundle() {
-        const mtime = Math.trunc(fs.statSync("scripts/pa.min.js.map").mtime.getTime() / 1000);
+        const signature = crypto.createHash("sha256").update(fs.readFileSync("scripts/pa.min.js.map")).digest("hex").substring(0, 12);
         const fd = fs.openSync("scripts/pa.min.js", "r+");
         const sz = fs.fstatSync(fd).size;
         const buf = Buffer.alloc(8192);
         const nr = fs.readSync(fd, buf, 0, 1000, Math.max(sz - 1000, 0));
         const s = buf.toString("utf8", 0, nr);
-        const m = s.match(/^([^]*\/\/\# sourceMappingURL=pa\.min\.js\.map)\s*$/);
+        const m = s.match(/^([^]*)(\/\/\# sourceMappingURL=pa\.min\.js\~?\.map)\s*$/);
         if (m) {
-            const nbuf = Buffer.from(m[1] + "?mtime=" + mtime + "\n", "utf8");
+            const nbuf = Buffer.from(m[1] + m[2].replace("~", "") + "?version=" + signature + "\n", "utf8");
             fs.writeSync(fd, nbuf, 0, nbuf.length, Math.max(sz - 1000, 0));
         }
         fs.closeSync(fd);
