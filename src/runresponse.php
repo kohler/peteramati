ebb20b03f06b9923736b5ee354988ee3bc10ad16
Eddie Kohler <ekohler@gmail.com>
2024-05-20 14:50:39 -0400

    Run responses include repogid and url.

--- src/runresponse.php
+++ src/runresponse.php
@@ -16,6 +16,10 @@ class RunResponse implements JsonSerializable {
     public $runner;
     /** @var int */
     public $repoid;
+    /** @var string */
+    public $repogid;
+    /** @var ?string */
+    public $url;
     /** @var ?string */
     public $hash;
     /** @var ?array */
@@ -62,6 +66,8 @@ class RunResponse implements JsonSerializable {
         $rr->pset = $runner->pset->urlkey;
         $rr->runner = $runner->name;
         $rr->repoid = $info->repo->repoid;
+        $rr->repogid = $info->repo->repogid;
+        $rr->url = $info->repo->url;
         $rr->hash = $info->hash();
         return $rr;
     }
@@ -69,37 +75,42 @@ class RunResponse implements JsonSerializable {
     /** @param object $x
      * @return ?RunResponse */
     static function make_log($x) {
-        if (is_string($x->pset ?? null) && is_string($x->runner ?? null)) {
-            $rr = new RunResponse;
-            $rr->pset = $x->pset;
-            $rr->runner = $x->runner;
-            $rr->repoid = is_int($x->repoid ?? null) ? $x->repoid : 0;
-            if (is_string($x->hash ?? null)) {
-                $rr->hash = $x->hash;
-            }
-            if (is_object($x->settings ?? null)) {
-                $rr->settings = (array) $x->settings;
-            }
-            if (isset($x->tags)) {
-                if (is_array($x->tags)) {
-                    $rr->tags = $x->tags;
-                } else if (is_string($x->tags)) {
-                    $rr->tags = [$x->tags];
-                }
-            }
-            if (is_int($x->timestamp ?? null)) {
-                $rr->timestamp = $x->timestamp;
-            }
-            if (is_int($x->queueid ?? null)) {
-                $rr->queueid = $x->queueid;
-            }
-            if (is_string($x->eventsource ?? null)) {
-                $rr->eventsource = $x->eventsource;
-            }
-            return $rr;
-        } else {
+        if (!is_string($x->pset ?? null) || !is_string($x->runner ?? null)) {
             return null;
         }
+        $rr = new RunResponse;
+        $rr->pset = $x->pset;
+        $rr->runner = $x->runner;
+        $rr->repoid = is_int($x->repoid ?? null) ? $x->repoid : 0;
+        if (is_string($x->repogid ?? null)) {
+            $rr->repogid = $x->repogid;
+        }
+        if (is_string($x->url ?? null)) {
+            $rr->url = $x->url;
+        }
+        if (is_string($x->hash ?? null)) {
+            $rr->hash = $x->hash;
+        }
+        if (is_object($x->settings ?? null)) {
+            $rr->settings = (array) $x->settings;
+        }
+        if (isset($x->tags)) {
+            if (is_array($x->tags)) {
+                $rr->tags = $x->tags;
+            } else if (is_string($x->tags)) {
+                $rr->tags = [$x->tags];
+            }
+        }
+        if (is_int($x->timestamp ?? null)) {
+            $rr->timestamp = $x->timestamp;
+        }
+        if (is_int($x->queueid ?? null)) {
+            $rr->queueid = $x->queueid;
+        }
+        if (is_string($x->eventsource ?? null)) {
+            $rr->eventsource = $x->eventsource;
+        }
+        return $rr;
     }
 
     /** @param string $tag
