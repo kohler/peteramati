06c64e2b0753256db0ca0b9656ce1e370deb29b2
Eddie Kohler <ekohler@gmail.com>
2025-10-07 17:39:35 -0400

    Actually enable .need-lineno attribute

--- scriptsrc/render.js
+++ scriptsrc/render.js
@@ -125,7 +125,7 @@ add_format({
     format: 3,
     render: function (text, context) {
         if (!md2) {
-            md2 = window.markdownit({highlight: try_highlight, linkify: true, html: true}).use(markdownit_katex).use(markdownit_attributes);
+            md2 = window.markdownit({highlight: try_highlight, linkify: true, html: true, attributes: true}).use(markdownit_katex).use(markdownit_attributes);
             md2.renderer.rules.link_open = render_link_open_3;
         }
         return md2.render(text);
